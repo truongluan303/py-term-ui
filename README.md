fb62291a903bf8e872ebcd21b7a1d09c04df2b0b
luantruong <truongluan303@gmail.com>
2022-09-24 01:50:29 -0700

    Update progress bar and add progress bar to doc

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Python Terminal UI Library
 
 A library for terminal UI components in Python
+
+---
+
+## Progress Bar
+
+The progress bar is initialized with a `max_val`, and it will be updated every time the `update` method is called. If you don't provide a parameter for the `update` method, the default value will be `1`.
+
+Sample usage:
+
+```python
+pb = ProgressBar(100)
+pb.start()
+for i in range(100):
+    # do something
+    pb.update()
+```
+
+Output:
+
+```
+Progress |█████████---------------------| [33.0%] in 6.7s
+```
+```
+Progress |██████████████████████████████| [100.0%] in 20.3s ✅
+```
--- pyterm/components/progress_bar.py
+++ pyterm/components/progress_bar.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Optional
+from time import time
 
 
 @dataclass
@@ -8,18 +10,30 @@ class ProgressBar:
     bar_length: int = 30
     filled_cell: str = "█"
     empty_cell: str = "-"
+    shows_elapsed_time: bool = True
 
     def __post_init__(self) -> None:
+        self._started = False
         self._current_val = -1
-        self.increment()
 
-    def increment(self, val: float = 1) -> None:
+    def start(self) -> None:
+        """
+        Start the progress
+        """
+        self._starttime = time()
+        self._started = True
+        self.update()
+
+    def update(self, val: float = 1) -> None:
         """
         Increase the progress.
 
         Args:
             `val`: How much more progress has been made. Defaults to 1.
         """
+        if not self._started:
+            self.start()
+
         if self.is_done:
             return
 
@@ -31,12 +45,21 @@ class ProgressBar:
 
         bar = self.filled_cell * filled_len + self.empty_cell * empty_len
         percent = round(100.0 * self._current_val / self.max_val, 1)
-        
+
         print(
-            f"\r{self.title} |{bar}| {percent}%",
+            f"\r{self.title} |{bar}| [{percent}%]"
+            + (
+                f" in {round(time() - self._starttime, 1)}s"
+                if self.shows_elapsed_time
+                else ""
+            ),
             end="\r" if not self.is_done else " ✅\n",
         )
 
     @property
     def is_done(self) -> bool:
         return self._current_val >= self.max_val
+
+    @property
+    def starttime(self) -> Optional[float]:
+        return self._starttime if self._started else None
